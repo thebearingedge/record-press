93ae1a3bdb3eb8d7752e925b7a720bde32eb643f
Tim Davis <contact@timdav.is>
2020-03-28 20:05:59 -0700

    Only generate record keys once.

--- src/index.ts
+++ src/index.ts
@@ -26,56 +26,48 @@ type Options<R> = {
 
 class Indexer<R> {
 
-  indexed: number
-  private readonly keyGens: Array<KeyGen<R>>
   private readonly indexes: Array<Record<string, boolean>>
 
   constructor(uniqueBy: Array<Unique<R>>) {
-    this.indexed = 0
     this.indexes = uniqueBy.map(() => Object.create(null))
-    this.keyGens = uniqueBy.map(unique => {
-      if (typeof unique === 'function') return unique
-      if (typeof unique === 'string') {
-        return (record: R) => JSON.stringify(record[unique])
-      }
-      return (record: R) => JSON.stringify(unique.map(key => record[key]))
-    })
   }
 
-  has(record: R): boolean {
-    return this.keyGens.some((keyGen, index) => {
-      const key = keyGen(record)
-      return this.indexes[index][key]
-    })
+  has(keys: string[]): boolean {
+    return this.indexes.some((keyed, index) => keyed[keys[index]])
   }
 
-  add(record: R): void {
-    this.indexed++
-    this.keyGens.forEach((keyGen, index) => {
-      const key = keyGen(record)
-      this.indexes[index][key] = true
-    })
+  add(keys: string[]): void {
+    this.indexes.forEach((keyed, index) => { keyed[keys[index]] = true })
   }
 
 }
 
 export = function recordPress<R extends Record<string, Data>>(config: Config<R>) {
   const { factory, uniqueBy = [] } = config
+  const keyGens = uniqueBy.map(unique => {
+    if (typeof unique === 'function') return unique
+    if (typeof unique === 'string') {
+      return (record: R) => JSON.stringify(record[unique])
+    }
+    return (record: R) => JSON.stringify(unique.map(key => record[key]))
+  })
   return function * generator(options?: Options<R>) {
     const indexer = new Indexer(uniqueBy)
     const { count = 1, retries = 5000, override } = options ?? {}
     let retry = 0
-    while (indexer.indexed < count) {
-      if (retry === retries) {
-        throw new Error(`record press generator exceeded ${retries} retries`)
-      }
+    let succeeded = 0
+    while (succeeded < count) {
       const record = Object.assign(factory(), override)
-      if (indexer.has(record)) {
-        retry++
+      const keys = keyGens.map(keyGen => keyGen(record))
+      if (!indexer.has(keys)) {
+        succeeded++
+        indexer.add(keys)
+        yield record
         continue
       }
-      indexer.add(record)
-      yield record
+      if (++retry > retries) {
+        throw new Error(`record press generator exceeded ${retries} retries`)
+      }
     }
   }
 }
